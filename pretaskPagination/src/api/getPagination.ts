59757742c73e6ed846cc968baa08f01faf02b75f
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-03 23:38:40 +0900

    [ feat ] api 통신

---
+++ pretaskPagination/src/api/getPagination.ts
@@ -0,0 +1,11 @@
+//api 통신
+
+export async function getPagination(props: url) {
+  // const {url} = props;
+    // const response = await axios.get( url,
+      // {
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      // },
+}
